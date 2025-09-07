0c64e8131781162bb86fb0e986f348dc84f7663d
dingjun <28059535@qq.com>
2025-09-07 23:19:29 +0800

    删除build目录，减小工程代码所占用的磁盘空间

--- build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/dingjun/workspace/esp32/esp-idf/components/mbedtls/mbedtls/library/error.c
--- build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
+++
@@ -1,1 +0,0 @@
-/home/dingjun/workspace/esp32/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
